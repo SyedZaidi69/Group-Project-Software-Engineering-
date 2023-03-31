e248fd446fd12951dc8bab9907cb3069ba2529b0
SyedZaidi69 <zaidis4@roehampton.ac.uk>
2023-03-31 11:35:02 +0100

    Deleting some useless files

--- src/test/index.test.js
+++
@@ -1,6 +0,0 @@
-import { functions } from "./tests";
-
-test("Object assignment", () => {
-  expect(functions.isExpressDefined()).toBeDefined();
-  expect(functions.isPrismaDefined()).toBeDefined();
-});
